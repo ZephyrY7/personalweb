7825fbe2e0580686b4bcdf4a68d8ffe736c2a726
ZephyrY7 <leeenhao2@gmail.com>
2024-04-23 10:15:12 +0800

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,11 @@
-"use strict";
-
 // Element toggle function
-const elementToggleFunc = function (elem) {
+const toggleActive = (elem, activeText, inactiveText) => {
   elem.classList.toggle("active");
   if (elem === sidebar) {
     const buttonText = elem.querySelector("span");
     const icon = elem.querySelector("ion-icon");
-    buttonText.textContent = elem.classList.contains("active")
-      ? "Hide Contacts"
-      : "Show Contacts";
-    icon.style.transform = elem.classList.contains("active")
-      ? "rotate(180deg)"
-      : "rotate(0deg)";
+    buttonText.textContent = elem.classList.contains("active") ? activeText : inactiveText;
+    icon.style.transform = elem.classList.contains("active") ? "rotate(180deg)" : "rotate(0deg)";
   }
 };
 
@@ -20,68 +14,41 @@ const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
 // Sidebar toggle functionality for mobile
-sidebarBtn.addEventListener("click", function () {
-  elementToggleFunc(sidebar);
-});
+sidebarBtn.addEventListener("click", () => toggleActive(sidebar, "Hide Contacts", "Show Contacts"));
 
 // Custom select variables
 const select = document.querySelector("[data-select]");
-const selectValue = document.querySelector("[data-select-value]");
-
-select.addEventListener("click", function () {
-  elementToggleFunc(this);
-});
+select.addEventListener("click", () => toggleActive(select));
 
 // Filter variables
-const filterListBtns = document.querySelectorAll(
-  ".filter-list [data-filter-btn]"
-);
-const filterSelectBtns = document.querySelectorAll(
-  ".select-list [data-select-item]"
-);
+const filterListBtns = document.querySelectorAll(".filter-list [data-filter-btn]");
+const filterSelectBtns = document.querySelectorAll(".select-list [data-select-item]");
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
-const filterFunc = function (selectedValue) {
-  filterListBtns.forEach((btn) => {
-    btn.classList.toggle(
-      "active",
-      btn.innerText.toLowerCase() === selectedValue
-    );
-  });
+const filterFunc = (selectedValue) => {
+  const toggleActiveFilter = (btn) =>
+    btn.classList.toggle("active", btn.innerText.toLowerCase() === selectedValue);
+  filterListBtns.forEach(toggleActiveFilter);
+  filterSelectBtns.forEach(toggleActiveFilter);
 
-  filterSelectBtns.forEach((btn) => {
-    btn.classList.toggle(
-      "active",
-      btn.innerText.toLowerCase() === selectedValue
-    );
-  });
-
-  filterItems.forEach((item) => {
+  filterItems.forEach((item) =>
     item.classList.toggle(
       "active",
       selectedValue === "all" || selectedValue === item.dataset.category
-    );
-  });
+    )
+  );
 
   // Update selectValue with the correct casing
-  selectValue.innerText = selectedValue;
+  document.querySelector("[data-select-value]").innerText = selectedValue;
 
   // Close the filter list
   select.classList.remove("active");
 };
 
 // Add event in all filter list and select button items
-const allFilterButtons = document.querySelectorAll(
-  ".filter-list [data-filter-btn], .select-list [data-select-item]"
-);
-allFilterButtons.forEach((btn) => {
-  btn.addEventListener("click", function () {
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText =
-      selectedValue.charAt(0).toUpperCase() + selectedValue.slice(1);
-    filterFunc(selectedValue);
-  });
-});
+document
+  .querySelectorAll(".filter-list [data-filter-btn], .select-list [data-select-item]")
+  .forEach((btn) => btn.addEventListener("click", () => filterFunc(btn.innerText.toLowerCase())));
 
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
@@ -89,50 +56,43 @@ const pages = document.querySelectorAll("[data-page]");
 
 // Add event to all nav links
 navigationLinks.forEach((link) => {
-  link.addEventListener("click", function () {
-    const selectedPage = this.innerHTML.toLowerCase();
-    let isActive = false; // Define isActive outside the loop
-    pages.forEach((page) => {
-      isActive = selectedPage === page.dataset.page;
-      page.classList.toggle("active", isActive);
-    });
-    navigationLinks.forEach((navLink) => {
-      isActive = selectedPage === navLink.innerHTML.toLowerCase();
-      navLink.classList.toggle("active", isActive);
-    });
+  link.addEventListener("click", () => {
+    const selectedPage = link.innerHTML.toLowerCase();
+    pages.forEach((page) => page.classList.toggle("active", selectedPage === page.dataset.page));
+    navigationLinks.forEach((navLink) =>
+      navLink.classList.toggle("active", selectedPage === navLink.innerHTML.toLowerCase())
+    );
     window.scrollTo(0, 0);
   });
 });
 
 // POPUP
 const popup = document.getElementById("popup");
-const popupContentContainer = document.getElementById(
-  "popup-content-container"
-);
+const popupContentContainer = document.getElementById("popup-content-container");
 
-function showHtmlPopup(htmlFile, button) {
-  fetch(htmlFile)
-    .then((response) => response.text())
-    .then((htmlContent) => {
-      popupContentContainer.innerHTML = htmlContent;
-      const rect = button.getBoundingClientRect();
-      const buttonCenterX = rect.left + rect.width / 2;
-      const buttonCenterY = rect.top + rect.height / 2;
-      const scaleX = window.innerWidth / rect.width;
-      const scaleY = window.innerHeight / rect.height;
-      popup.style.display = "flex";
-      popup.style.animation = "fadeInBackground 0.5s ease forwards";
-      popupContentContainer.style.transformOrigin = `${buttonCenterX}px ${buttonCenterY}px`;
-      popupContentContainer.style.transform = `scale(${1 / scaleX}, ${
-        1 / scaleY
-      })`;
-      setTimeout(() => {
-        popupContentContainer.style.transition = "transform 0.5s";
-        popupContentContainer.style.transform = "scale(1, 1)";
-      }, 50);
+const showHtmlPopup = async (htmlFile, button) => {
+  const response = await fetch(htmlFile);
+  const htmlContent = await response.text();
+  popupContentContainer.innerHTML = htmlContent;
+  const { left, top, width, height } = button.getBoundingClientRect();
+  const [buttonCenterX, buttonCenterY] = [left + width / 2, top + height / 2];
+  const [scaleX, scaleY] = [window.innerWidth / width, window.innerHeight / height];
+  Object.assign(popup.style, {
+    display: "flex",
+    animation: "fadeInBackground 0.5s ease forwards",
+  });
+  Object.assign(popupContentContainer.style, {
+    transformOrigin: `${buttonCenterX}px ${buttonCenterY}px`,
+    transform: `scale(${1 / scaleX}, ${1 / scaleY})`,
+  });
+  setTimeout(() => {
+    Object.assign(popupContentContainer.style, {
+      transition: "transform 0.5s",
+      transform: "scale(1, 1)",
     });
+  }, 50);
   document.body.style.overflow = "hidden";
-}
+};
 
 popup.addEventListener("click", (event) => {
   if (event.target === popup) {
@@ -140,36 +100,44 @@ popup.addEventListener("click", (event) => {
   }
 });
 
-function hideHtmlPopup() {
-  popup.style.animation = "fadeOutBackground 0.7s ease forwards";
-  popupContentContainer.style.animation = "scaleDown 0.7s ease forwards";
+const hideHtmlPopup = () => {
+  Object.assign(popup.style, {
+    animation: "fadeOutBackground 0.7s ease forwards",
+  });
+  Object.assign(popupContentContainer.style, {
+    animation: "scaleDown 0.7s ease forwards",
+  });
   setTimeout(() => {
-    popup.style.display = "none";
-    popupContentContainer.style.transform = "scale(1)";
-    popupContentContainer.style.transition = "none";
-    popup.style.animation = "";
-    popupContentContainer.style.animation = "";
+    Object.assign(popup.style, { display: "none", animation: "" });
+    Object.assign(popupContentContainer.style, {
+      transform: "scale(1)",
+      transition: "none",
+      animation: "",
+    });
   }, 500);
   document.body.style.overflow = "auto";
-}
+};
 
-// Color mode
-function setTheme(theme) {
-  const themeToggle = document.querySelector(".theme-switch__checkbox");
-  themeToggle.checked = theme === "dark-mode"; // Update the checked state of the toggle switch
-  document.documentElement.className = theme;
-}
-
-const prefersDarkMode = window.matchMedia(
-  "(prefers-color-scheme: dark)"
-).matches;
-setTheme(prefersDarkMode ? "dark-mode" : "");
-
+// Cache DOM elements
 const themeToggle = document.querySelector(".theme-switch__checkbox");
-themeToggle.addEventListener("change", () => {
-  if (themeToggle.checked) {
-    setTheme("dark-mode");
-  } else {
-    setTheme("");
-  }
-});
+
+// Function to get CSS variable value
+const getCssVarValue = (varName) =>
+  getComputedStyle(document.documentElement).getPropertyValue(varName).trim();
+
+// Function to set theme
+const setTheme = (theme) => {
+  themeToggle.checked = theme === "dark-mode";
+  document.documentElement.className = theme;
+};
+
+// Check user's preferred color scheme
+const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+// Set initial theme based on user's preference
+setTheme(prefersDarkMode ? "dark-mode" : "light-mode");
+
+// Add event listener to theme toggle switch
+themeToggle.addEventListener("change", () =>
+  setTheme(themeToggle.checked ? "dark-mode" : "light-mode")
+);
