d52a996c436ff405fba750bcc4733c299599e8d8
ZephyrY7 <leeenhao2@gmail.com>
2024-12-22 10:55:24 +0800

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,159 +1,172 @@
-'use strict';
+// Element toggle function
+const toggleActive = (elem, activeText, inactiveText) => {
+  elem.classList.toggle("active");
+  if (elem === sidebar) {
+    const buttonText = elem.querySelector("span");
+    const icon = elem.querySelector("ion-icon");
+    buttonText.textContent = elem.classList.contains("active")
+      ? activeText
+      : inactiveText;
+    icon.style.transform = elem.classList.contains("active")
+      ? "rotate(180deg)"
+      : "rotate(0deg)";
+  }
+};
 
-
-
-// element toggle function
-const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
-
-// sidebar variables
+// Sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
-// sidebar toggle functionality for mobile
-sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
+// Sidebar toggle functionality for mobile
+sidebarBtn.addEventListener("click", () =>
+  toggleActive(sidebar, "Hide Contacts", "Show Contacts")
+);
 
-
-
-// testimonials variables
-const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
-const modalContainer = document.querySelector("[data-modal-container]");
-const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
-const overlay = document.querySelector("[data-overlay]");
-
-// modal variable
-const modalImg = document.querySelector("[data-modal-img]");
-const modalTitle = document.querySelector("[data-modal-title]");
-const modalText = document.querySelector("[data-modal-text]");
-
-// modal toggle function
-const testimonialsModalFunc = function () {
-  modalContainer.classList.toggle("active");
-  overlay.classList.toggle("active");
-}
-
-// add click event to all modal items
-for (let i = 0; i < testimonialsItem.length; i++) {
-
-  testimonialsItem[i].addEventListener("click", function () {
-
-    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
-
-    testimonialsModalFunc();
-
-  });
-
-}
-
-// add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
-
-
-
-// custom select variables
+// Custom select variables
 const select = document.querySelector("[data-select]");
-const selectItems = document.querySelectorAll("[data-select-item]");
-const selectValue = document.querySelector("[data-selecct-value]");
-const filterBtn = document.querySelectorAll("[data-filter-btn]");
+select.addEventListener("click", () => toggleActive(select));
 
-select.addEventListener("click", function () { elementToggleFunc(this); });
-
-// add event in all select items
-for (let i = 0; i < selectItems.length; i++) {
-  selectItems[i].addEventListener("click", function () {
-
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    elementToggleFunc(select);
-    filterFunc(selectedValue);
-
-  });
-}
-
-// filter variables
+// Filter variables
+const filterListBtns = document.querySelectorAll(
+  ".filter-list [data-filter-btn]"
+);
+const filterSelectBtns = document.querySelectorAll(
+  ".select-list [data-select-item]"
+);
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
-const filterFunc = function (selectedValue) {
+const filterFunc = (selectedValue) => {
+  const toggleActiveFilter = (btn) =>
+    btn.classList.toggle(
+      "active",
+      btn.innerText.toLowerCase() === selectedValue
+    );
+  filterListBtns.forEach(toggleActiveFilter);
+  filterSelectBtns.forEach(toggleActiveFilter);
 
-  for (let i = 0; i < filterItems.length; i++) {
+  filterItems.forEach((item) =>
+    item.classList.toggle(
+      "active",
+      selectedValue === "all" || selectedValue === item.dataset.category
+    )
+  );
 
-    if (selectedValue === "all") {
-      filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
-      filterItems[i].classList.add("active");
-    } else {
-      filterItems[i].classList.remove("active");
-    }
+  // Update selectValue with the correct casing
+  document.querySelector("[data-select-value]").innerText = selectedValue;
 
-  }
+  // Close the filter list
+  select.classList.remove("active");
+};
 
-}
-
-// add event in all filter button items for large screen
-let lastClickedBtn = filterBtn[0];
-
-for (let i = 0; i < filterBtn.length; i++) {
-
-  filterBtn[i].addEventListener("click", function () {
-
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    filterFunc(selectedValue);
-
-    lastClickedBtn.classList.remove("active");
-    this.classList.add("active");
-    lastClickedBtn = this;
-
-  });
-
-}
-
-
-
-// contact form variables
-const form = document.querySelector("[data-form]");
-const formInputs = document.querySelectorAll("[data-form-input]");
-const formBtn = document.querySelector("[data-form-btn]");
-
-// add event to all form input field
-for (let i = 0; i < formInputs.length; i++) {
-  formInputs[i].addEventListener("input", function () {
-
-    // check form validation
-    if (form.checkValidity()) {
-      formBtn.removeAttribute("disabled");
-    } else {
-      formBtn.setAttribute("disabled", "");
-    }
-
-  });
-}
-
-
+// Add event in all filter list and select button items
+document
+  .querySelectorAll(
+    ".filter-list [data-filter-btn], .select-list [data-select-item]"
+  )
+  .forEach((btn) =>
+    btn.addEventListener("click", () => filterFunc(btn.innerText.toLowerCase()))
+  );
 
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
-// add event to all nav link
-for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
-
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
-      } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
-      }
-    }
-
+// Add event to all nav links
+navigationLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    const selectedPage = link.innerHTML.toLowerCase();
+    pages.forEach((page) =>
+      page.classList.toggle("active", selectedPage === page.dataset.page)
+    );
+    navigationLinks.forEach((navLink) =>
+      navLink.classList.toggle(
+        "active",
+        selectedPage === navLink.innerHTML.toLowerCase()
+      )
+    );
+    window.scrollTo(0, 0);
   });
-}
+});
+
+// POPUP
+const popup = document.getElementById("popup");
+const popupContentContainer = document.getElementById(
+  "popup-content-container"
+);
+
+const showHtmlPopup = async (htmlFile, button) => {
+  const response = await fetch(htmlFile);
+  const htmlContent = await response.text();
+  popupContentContainer.innerHTML = htmlContent;
+  const { left, top, width, height } = button.getBoundingClientRect();
+  const [buttonCenterX, buttonCenterY] = [left + width / 2, top + height / 2];
+  const [scaleX, scaleY] = [
+    window.innerWidth / width,
+    window.innerHeight / height,
+  ];
+  Object.assign(popup.style, {
+    display: "flex",
+    animation: "fadeInBackground 0.5s ease forwards",
+  });
+  Object.assign(popupContentContainer.style, {
+    transformOrigin: `${buttonCenterX}px ${buttonCenterY}px`,
+    transform: `scale(${1 / scaleX}, ${1 / scaleY})`,
+  });
+  setTimeout(() => {
+    Object.assign(popupContentContainer.style, {
+      transition: "transform 0.5s",
+      transform: "scale(1, 1)",
+    });
+  }, 50);
+  document.body.style.overflow = "hidden";
+};
+
+popup.addEventListener("click", (event) => {
+  if (event.target === popup) {
+    hideHtmlPopup();
+  }
+});
+
+const hideHtmlPopup = () => {
+  Object.assign(popup.style, {
+    animation: "fadeOutBackground 0.7s ease forwards",
+  });
+  Object.assign(popupContentContainer.style, {
+    animation: "scaleDown 0.7s ease forwards",
+  });
+  setTimeout(() => {
+    Object.assign(popup.style, { display: "none", animation: "" });
+    Object.assign(popupContentContainer.style, {
+      transform: "scale(1)",
+      transition: "none",
+      animation: "",
+    });
+  }, 500);
+  document.body.style.overflow = "auto";
+};
+
+// Cache DOM elements
+const themeToggle = document.querySelector(".theme-switch__checkbox");
+
+// Function to get CSS variable value
+const getCssVarValue = (varName) =>
+  getComputedStyle(document.documentElement).getPropertyValue(varName).trim();
+
+// Function to set theme
+const setTheme = (theme) => {
+  themeToggle.checked = theme === "dark-mode";
+  document.documentElement.className = theme;
+};
+
+// Check user's preferred color scheme
+const prefersDarkMode = window.matchMedia(
+  "(prefers-color-scheme: dark)"
+).matches;
+
+// Set initial theme based on user's preference
+setTheme(prefersDarkMode ? "dark-mode" : "light-mode");
+
+// Add event listener to theme toggle switch
+themeToggle.addEventListener("change", () =>
+  setTheme(themeToggle.checked ? "dark-mode" : "light-mode")
+);
